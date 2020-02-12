98d91254ec2ed5dec6201772a96ee27c0f2d4c28
jac <crowja@gmail.com>
2020-02-11 19:16:07 -0600

    Somehow missed in the original commit.

---
+++ ntalign.h
@@ -0,0 +1,3 @@
+double ntalign_local(size_t lena, char *a, size_t lenb, char *b, double indel, double mismatch, double *wrk);
+double ntalign_global(size_t lena, char *a, size_t lenb, char *b, double indel, double mismatch, double *wrk);
+
